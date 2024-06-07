ea1c772f2c35e279faa6e88d2feead99dc9cd54a
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-06-07 22:40:24 +0300

    added counterReducer.ts

---
+++ src/reducers/counterReducer.ts
@@ -0,0 +1,83 @@
+
+export type StateType = {
+   currentValue: number
+   maxValue: number
+   startValue: number
+   clueText: string | null
+   errorText: string
+}
+
+
+export const counterReducer = (state: StateType, action: ActionsType): StateType => {
+   switch (action.type) {
+      case "INCREMENT": {
+         return {
+            ...state,
+            currentValue: state.currentValue + 1
+         };
+      }
+      case "RESET": {
+         return {
+            ...state,
+            currentValue: state.startValue
+         }
+      }
+      case "UPDATE_LIMITS": {
+         const {key, value} = action.payload;
+         return {
+            ...state,
+            [key]: value,
+            currentValue: -1,
+            clueText: "enter values and press 'set'",
+         }
+      }
+      case "SET_LIMITS": {
+         return {
+            ...state,
+            currentValue: state.startValue,
+            clueText: null
+         }
+      }
+      default: {
+         return state;
+      }
+   }
+};
+
+
+
+type IncrementCounterType = ReturnType<typeof incrementCounter>;
+export const incrementCounter = () => {
+   return {
+      type: "INCREMENT"
+   } as const;
+};
+
+type ResetCounterType = ReturnType<typeof resetCounter>;
+export const resetCounter = () => {
+  return {
+      type: "RESET"
+  } as const;
+};
+
+type UpdateCounterLimitsType = ReturnType<typeof updateCounterLimits>;
+export const updateCounterLimits = (key: "startValue" | "maxValue", value: number) => {
+   return {
+      type: "UPDATE_LIMITS",
+      payload: {
+         key,
+         value,
+      }
+   } as const;
+};
+
+type SetCounterLimitsType = ReturnType<typeof setCounterLimits>;
+export const setCounterLimits = () => {
+   return {
+      type: "SET_LIMITS"
+   } as const;
+};
+
+// ActionsType for counterReducer
+type ActionsType = IncrementCounterType | ResetCounterType
+   | UpdateCounterLimitsType | SetCounterLimitsType;
